2c41da6c5e9dead86a9637213267b81003eb71cf
joshwood <josh.norwood@gmail.com>
2014-05-21 15:33:59 -0500

    added better proxy handling. comments.

--- app/controllers/iRiverController.js
+++ app/controllers/iRiverController.js
@@ -2,18 +2,30 @@
 
 var request = require('request');
 var parseString = require('xml2js').parseString;
+var util = require('util');
 
+var stateDataURL = 'http://www.darrenchaney.com/%sdata.xml';
+var gageDataURL = 'http://water.weather.gov/ahps2/hydrograph_to_xml.php?output=xml&gage=%s';
+
+var _proxy;
+
+/**
+ * initializes the module - currently just a proxy
+ */
+exports.init = function(proxy){
+	_proxy = proxy;
+}
 
 /**
  * controlls lookup of available river gages by state
  */
 exports.riversByState = function(req, res){
 	var stateId = req.params.stateId;
-	request({
-				url: 'http://www.darrenchaney.com/'+stateId+'data.xml'
-			}, 
+	// make the request to get the rivers in a state
+	request(stateRequest(stateId), 
 			function (error, response, body) {
 				if (!error && response.statusCode == 200) {
+					// if we got a response we convert the response to json
 					parseString(body, function(err, result){
 						res.jsonp(result);
 					});
@@ -30,11 +42,11 @@ exports.riversByState = function(req, res){
  */
 exports.gageById = function (req, res){
 	var gageId = req.params.gageId;
-	request({
-				url: 'http://water.weather.gov/ahps2/hydrograph_to_xml.php?output=xml&gage='+gageId
-			}, 
+	// make
+	request(gageRequest(gageId), 
 			function (error, response, body) {
 				if (!error && response.statusCode == 200) {
+					// if we got a response we convert the response to json
 					parseString(body, function(err, result){
 						res.jsonp(result);
 					});
@@ -45,3 +57,27 @@ exports.gageById = function (req, res){
 			}
 	);
 };
+
+/**
+ * helper method to format our request based on state param and the presense of
+ * a proxy var
+ */
+function stateRequest(stateId){
+	var req = {
+		url: util.format(stateDataURL, stateId)
+	};
+	if(_proxy) req.proxy = _proxy;
+	return req;
+}
+
+/**
+ * helper method to format our request based on gage param and the presense of
+ * a proxy var
+ */
+function gageRequest(gageId){
+	var req = {
+		url: util.format(gageDataURL, gageId)
+	};
+	if(_proxy) req.proxy = _proxy;
+	return req;
+}
--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var app = express();
 
+/*
+ * if you're unlucky enough to be behind a firewall, set HTTP_PROXY in 
+ * your environment before starting node server
+ */
+var proxy = process.env.HTTP_PROXY || null;
+
+/*
+ * required for json post - which we're not currently using.
+ * this MUST be located here before router middleware - ugh, that was 2 hours of wasted time
+ * http://stackoverflow.com/questions/20381059/cant-get-post-body-from-request-using-express-js
+ */
 app.use(express.bodyParser());
 
 /*
  * controller setup, do this prior to routes
  */
-require('./app/controllers/iRiverController');
+require('./app/controllers/iRiverController').init(proxy);
 
 /*
  * express routes, must have controller config first
